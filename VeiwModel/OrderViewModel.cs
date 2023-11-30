c32ba49798a5380c66b3dc39d0b70fd3f35368c7
Hesham Mohamed <heshammohamed.netdeveloper@gmail.com>
2023-11-30 19:02:30 +0200

    upload

---
+++ VeiwModel/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VersionOfProject.VeiwModel
+{
+    public class OrderViewModel
+    {
+        public DateTime OrderDate { get; set; }
+        public int CountItems { get; set; }
+        public decimal Price { get; set; }
+    }
+}
